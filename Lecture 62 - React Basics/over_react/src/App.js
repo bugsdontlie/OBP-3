bc054056f2bb379cd0981cd922e865a2ad43b4f8
ACCIOJOB <acciojob@ACCIOJOBs-MacBook-Pro.local>
2026-01-13 13:31:32 +0530

    react basics

---
+++ Lecture 62 - React Basics/over_react/src/App.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {/* <Counter /> */}
+        {Counter()}
+      </header>
+    </div>
+  );
+}
+
+export default App;
